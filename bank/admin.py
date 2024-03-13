3335d64f25e9aa6866644af1234093abd13054e1
kudakwashe chris chipangura <49289670+kudah99@users.noreply.github.com>
2024-03-13 23:25:46 +0200

    Added banks

--- bank/admin.py
+++ bank/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from .models import Bank
 
-# Register your models here.
+class BankResource(resources.ModelResource):
+    class Meta:
+        model = Bank
+
+class BankAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('id','logo','name', 'services','link', 'created_at', 'updated_at')
+    search_fields = ('name',)
+
+    resource_class = BankResource
+
+admin.site.register(Bank, BankAdmin)
--- bank/models.py
+++ bank/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django_resized import ResizedImageField
+from django.urls import reverse
 
 # Create your models here.
+class Bank(models.Model):
+    name = models.CharField(max_length=100)
+    services = models.TextField()
+    logo = models.ImageField(upload_to='images/', null=True)
+    link = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
