14633fe3232ec863e86f3fa614b724d15c42d581
kudakwashe chris chipangura <49289670+kudah99@users.noreply.github.com>
2024-03-13 23:21:56 +0200

    Resolved databse issues

--- coarse/models.py
+++ coarse/models.py
@@ -6,13 +6,11 @@ from django.urls import reverse
 class Coarse(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    image = ResizedImageField(size=[500, 500], upload_to='images/', null=True)
+    image = models.ImageField(upload_to='images/', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
     
-    def get_absolute_url(self):
-        return reverse("coarse", kwargs={"id": self.pk})
     
