b09cc54195932159031d797d8e28098255766f01
kudakwashe chris chipangura <49289670+kudah99@users.noreply.github.com>
2024-04-09 19:28:07 +0200

    Added aws s3 storage

--- coarse/models.py
+++ coarse/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django_resized import ResizedImageField
 from django.urls import reverse
+from app.storage_backend import PublicMediaStorage
 
 # Create your models here.
 class Coarse(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='images/', null=True)
+    image = models.ImageField(storage=PublicMediaStorage(), null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- coarse_content/models.py
+++ coarse_content/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from coarse.models import Coarse
 from martor.models import MartorField
+from app.storage_backend import PublicMediaStorage
 
 # Create your models here.
 class CoarseContent(models.Model):
     name = models.CharField(max_length=100)
     notes = MartorField()
-    video = models.FileField(upload_to='coarse_content/', null=True)
+    video = models.FileField(storage=PublicMediaStorage(), null=True)
     coarse = models.ForeignKey(Coarse,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
